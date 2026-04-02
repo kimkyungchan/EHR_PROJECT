78d67305618e8628aa09a22fe1ddf71d6baa7bc9
ASUS <ASUS@DESKTOP-U9KP54U>
2026-04-02 23:36:59 +0900

    mvc 패턴 및 api 패턴 학습

--- EHR_PROJECT/src/main/java/com/ehr/ehr_project/controller/EhrController.java
+++ EHR_PROJECT/src/main/java/com/ehr/ehr_project/controller/EhrController.java
@@ -3,13 +3,48 @@ package com.ehr.ehr_project.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class EhrController {
+	
+	static class Hello {
+		private String name;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+	}
 
 	@GetMapping("hello")
 	public String hello(Model model) {
 		model.addAttribute("data", "hello!!!");
 		return "hello";
+	}
+	
+	@GetMapping("hello_mvc")
+	public String hello_mvc(@RequestParam("name") String name, Model model) {
+		model.addAttribute("name", name);
+		return "hello-template";
+	}
+	
+	@GetMapping("hello_string")
+	@ResponseBody
+	public String hello_string(@RequestParam("name") String name) {
+		return "hello-template"+name;
+	}
+	
+	@GetMapping("hello_api")
+	@ResponseBody
+	public Hello hello_api(@RequestParam("name") String name) {
+		Hello hello = new Hello();
+		hello.setName(name);
+		
+		return hello;
 	}
 }
